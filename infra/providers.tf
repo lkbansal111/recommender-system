ae03ca5f656fcd3b7cb3259aff865ab589262b0b
Your Name <you@example.com>
2025-08-10 03:52:46 +0000

    changes

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-# Token for Kubernetes provider
+# Token to talk to the EKS API as your Jenkins IAM identity
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
@@ -11,5 +11,5 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
+  # NOTE: remove load_config_file line (not supported in this provider)
 }
